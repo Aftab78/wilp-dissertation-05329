99d721615a2806a70b0d381ae0e5a0d366b57d81
Aftab Hassan <aftab.hassan7@gmail.com>
2025-02-25 03:37:14 +0530

    Deploy

--- function_app.py
+++ function_app.py
@@ -4,55 +4,55 @@ import joblib
 import json
 import pandas as pd
 
-app = func.FunctionApp(auth_level=func.AuthLevel.FUNCTION)
-
 # Load the model at the start so that it doesn't need to be loaded on every request
-model = joblib.load('rfr_model.pkl')
+#model = joblib.load('rfr_model.pkl')
 
-@app.route(route="wilp_models")
-def wilp_models(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-    try:
-        req_body = req.get_json()
-    except ValueError:
-        return func.HttpResponse(
-            "Invalid request body. Please pass valid JSON data.", 
-            status_code=400
-        )
-    
-    if 'data' not in req_body:
-        return func.HttpResponse(
-            "Please provide the input data in the request body as a 'data' field.", 
-            status_code=400
-        )
-
-    try:
-        input_data = req_body['data']
-        df = pd.DataFrame(input_data)
-        df['year'] = df['ALL_DATE'].apply(lambda x: int(x[-4:]))
-        df['month'] = df['ALL_DATE'].apply(lambda x: int(x[3:5]))
-        df['day'] = df['ALL_DATE'].apply(lambda x: int(x[:2]))  
-        
-        x = df[['year', 'month', 'day']]
-        predictions = model.predict(x)
-
-        result = {
-            "predictions": predictions.tolist()
-        }
-
-        return func.HttpResponse(
-            json.dumps(result),
-            status_code=200,
-            mimetype="application/json"
-        )
-    except Exception as e:
-        logging.error(f"Error during prediction: {e}")
-        return func.HttpResponse(
-            "An error occurred while performing prediction. Please check your input data and try again.",
-            status_code=500
-        )
-
+# @app.route(route="wilp_models")
+# def wilp_models(req: func.HttpRequest) -> func.HttpResponse:
+    # logging.info('Python HTTP trigger function processed a request.')
+# 
+    # try:
+        # req_body = req.get_json()
+    # except ValueError:
+        # return func.HttpResponse(
+            # "Invalid request body. Please pass valid JSON data.", 
+            # status_code=400
+        # )
+    # 
+    # if 'data' not in req_body:
+        # return func.HttpResponse(
+            # "Please provide the input data in the request body as a 'data' field.", 
+            # status_code=400
+        # )
+# 
+    # try:
+        # input_data = req_body['data']
+        # df = pd.DataFrame(input_data)
+        # df['year'] = df['ALL_DATE'].apply(lambda x: int(x[-4:]))
+        # df['month'] = df['ALL_DATE'].apply(lambda x: int(x[3:5]))
+        # df['day'] = df['ALL_DATE'].apply(lambda x: int(x[:2]))  
+        # 
+        # x = df[['year', 'month', 'day']]
+        # predictions = model.predict(x)
+# 
+        # result = {
+            # "predictions": predictions.tolist()
+        # }
+# 
+        # return func.HttpResponse(
+            # json.dumps(result),
+            # status_code=200,
+            # mimetype="application/json"
+        # )
+    # except Exception as e:
+        # logging.error(f"Error during prediction: {e}")
+        # return func.HttpResponse(
+            # "An error occurred while performing prediction. Please check your input data and try again.",
+            # status_code=500
+        # )
+# 
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:

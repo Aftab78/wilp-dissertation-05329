69ab3485ea668b961a8b337aecb4ab2a9affecdc
Aftab Hassan <aftab.hassan7@gmail.com>
2025-03-06 23:23:12 +0530

    update model

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Load the model at the start so that it doesn't need to be loaded on every request
 rfr_model = joblib.load('rfr_model.pkl')
-xgbr_model = joblib.load('xgbr_model.pkl')
+xgbr_model = joblib.load('expert_model.pkl')
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -32,9 +32,9 @@ def predict(req, model):
         df = pd.DataFrame(input_data)
         df['year'] = df['ALL_DATE'].apply(lambda x: int(x[-4:]))
         df['month'] = df['ALL_DATE'].apply(lambda x: int(x[3:5]))
-        df['day'] = df['ALL_DATE'].apply(lambda x: int(x[:2]))  
+        #df['day'] = df['ALL_DATE'].apply(lambda x: int(x[:2]))  
         
-        x = df[['year', 'month', 'day']]
+        x = df[['year', 'month']]
         predictions = model.predict(x)
 
         result = {

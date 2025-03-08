c63f15e78d378d9766b2126b935fac97c9cf6920
Aftab Hassan <aftab.hassan7@gmail.com>
2025-03-09 01:12:14 +0530

    XGBR DEployed

--- function_app.py
+++ function_app.py
@@ -4,17 +4,12 @@ import joblib
 import json
 import pandas as pd
 from xgboost import XGBRegressor
-#import tensorflow as tf
-#import os
-#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
 # Load the model at the start so that it doesn't need to be loaded on every request
 model1 = joblib.load('rfr_model.pkl')
 model2 = joblib.load('svr_model.pkl')
 model3 = XGBRegressor()
 model3.load_model("xgbr_model.json")
 
-#model4 = tf.keras.models.load_model("cnn_model.keras")
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -146,46 +141,3 @@ def xgbr_models(req: func.HttpRequest) -> func.HttpResponse:
             "An error occurred while performing prediction. Please check your input data and try again.",
             status_code=500
         )
-        
-# @app.route(route="cnn_models")
-# def cnn_models(req: func.HttpRequest) -> func.HttpResponse:
-    # logging.info('Python HTTP trigger cnn_model function processed a request.')
-# 
-    # try:
-        # req_body = req.get_json()
-    # except ValueError:
-        # return func.HttpResponse(
-            # "Invalid request body. Please pass valid JSON data.", 
-            # status_code=400
-        # )
-# 
-    # if 'data' not in req_body:
-        # return func.HttpResponse(
-            # "Please provide the input data in the request body as a 'data' field.", 
-            # status_code=400
-        # )
-# 
-    # try:
-        # input_data = req_body['data']
-        # df = pd.DataFrame(input_data)
-        # X_test = df["QTY"].values.reshape(-1, 1,1)
-# 
-        # predictions = model4.predict(X_test,verbose=0)
-        # predictions = predictions.reshape(-1,1)  
-        # predictions = predictions.reshape(-1)  
-# 
-        # result = {
-            # "predictions": predictions.tolist()
-        # }
-# 
-        # return func.HttpResponse(
-            # json.dumps(result),
-            # status_code=200,
-            # mimetype="application/json"
-        # )
-    # except Exception as e:
-        # logging.error(f"Error during prediction: {e}")
-        # return func.HttpResponse(
-            # "An error occurred while performing prediction. Please check your input data and try again.",
-            # status_code=500
-        # )
